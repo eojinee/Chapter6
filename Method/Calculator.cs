7498a4ca25f2fef721d4d8520b26122354521dd0
어진 김 <k91897599@gmail.com>
2023-03-20 15:28:53 +0900

    202303201528

---
+++ Method/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Calculator
+    {
+        public static int Plus(int a , int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a , int b)
+        {
+            return a - b;
+
+        }
+        class MainApp
+        {
+            public static void Main()
+            {
+                int result = Calculator.Plus(3, 4); //calculator에있는 plus를 3이랑 4에적용시킵니다
+                Console.WriteLine(result);
+
+                result = Calculator.Minus(5, 2);
+                Console.WriteLine(result);
+            }
+        }
+        
+       
+        }
+    }
+
